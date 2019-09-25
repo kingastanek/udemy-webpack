237e95aa9f6f1bd5f26b7d0e3f4485356eb361f5
kingastanek <kin.stanek@gmail.com>
2019-09-25 22:54:47 +0200

    Lazy Loading

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ module.exports = {
   devtool: "cheap-module-eval-source-map",
   entry: "./src/index.js",
   output: {
-    paht: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPaht: ""
+    chunkFilename: '[id].js',
+    publicPath: ""
   },
   resolve: {
     extensions: [".js", ".jsx"]
