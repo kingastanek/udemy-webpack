9c17a5f8a95c1fba7cc6722a070f70f1203df210
kingastanek <kin.stanek@gmail.com>
2019-09-25 21:28:37 +0200

    Adding file rules

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    paht: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPaht: ''
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
