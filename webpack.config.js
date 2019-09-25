d66cc60d09be61c1412453e661271dc6934d242f
kingastanek <kin.stanek@gmail.com>
2019-09-25 22:30:20 +0200

    Creating Rules for Images

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: './src/index.js',
+  devtool: "cheap-module-eval-source-map",
+  entry: "./src/index.js",
   output: {
-    paht: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPaht: ''
+    paht: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPaht: ""
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'style-loader' },
-          { 
-            loader: 'css-loader',
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
             options: {
               importLoaders: 1,
               modules: true,
-              localIdentName: '[name]__[local]__[hash:base64:5]'
+              localIdentName: "[name]__[local]__[hash:base64:5]"
             }
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              ident: 'postcss',
+              ident: "postcss",
               plugins: () => [
                 autoprefixer({
-                  browsers: [
-                    "> 1%",
-                    "last 2 versions"
-                  ]
+                  browsers: ["> 1%", "last 2 versions"]
                 })
               ]
             }
           }
-          
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]"
       }
     ]
   }
